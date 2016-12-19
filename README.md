d807e650bfcd597469afbf2d0e02d3f02c44bf3b
Tatiana Limache Quispe <Tati34che@gmail.com>
2016-12-19 16:38:06 -0500

    Ejercio Terminado

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ejercicio1
+Inicio : Promedio de 3 Numeros
+##Ingreso de Datos
+- Numero1
+- Numero2
+- Numero3
+- Suma
+- Promedio
+
+##Proceso
+- Pedir Numero1
+- Pedir Numero2
+- Pedir Numero3
+- Sumar Numero1 y Numero2 y Numero3 y almacenar en Suma
+- Dividir Suma entre 3 y almacenar en Promedio
+- Mostrar Promedio
---
+++ main.js
@@ -0,0 +1,12 @@
+var numero1=null;
+var numero2=null;
+var numero3=null;
+var suma=0;
+var promedio=0;
+numero1=prompt("Por favor ingresa un numero1");
+numero2=prompt("Por favor ingresa un numero2");
+numero3=prompt("Por favor ingresa un numero3");
+
+ suma=(parseFloat(numero1)+parseFloat(numero2)+parseFloat(numero3));
+ promedio=(parseFloat(suma)/3);
+  alert(promedio);
